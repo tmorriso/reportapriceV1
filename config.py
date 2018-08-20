26a10ecedadde8ec81f06909dcdbdfe8486fe58c
Tom Morrison <tmorriso1990@gmail.com>
2018-08-20 12:31:11 -0600

    heroku deployment changes round 2

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config(object):
 	'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	POSTS_PER_PAGE = 20
-	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-	# MAIL_SERVER = os.environ.get('MAIL_SERVER')
+
+	
+ #    MAIL_SERVER = os.environ.get('MAIL_SERVER')
  #    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
  #    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
  #    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
